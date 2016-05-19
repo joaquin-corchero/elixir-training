17d4c466c80263fe01e2ede23b06657657c88029
JoaquinCorcheroFMP <jcorchero@findmypast.com>
2016-05-19 14:26:55 +0100

    Added saved functionality but commented it

--- github_api/web/controllers/github_controller.ex
+++ github_api/web/controllers/github_controller.ex
@@ -4,11 +4,13 @@ defmodule GithubApi.GithubController do
   use GithubApi.Web, :controller
   alias GithubApi.Github.Client
   alias GithubApi.Github.Transform
+  alias GithubApi.Github.Users.Repository
 
   def user(conn, %{"user" => user}) do
     IEx.pry
     result = Client.get_user(user)
             |> Transform.extract_user
+            #|> Repository.save
 
     json conn, result
   end
