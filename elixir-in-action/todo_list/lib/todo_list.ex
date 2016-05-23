0d6593f115c2b24dbd375446dbda5ad97c189285
Joaquin Corchero <joaquin.corchero@gmail.com>
2016-05-23 23:12:11 +0100

    Added tests and functionality to create a new todo list and to add items

--- elixir-in-action/todo_list/lib/todo_list.ex
+++ elixir-in-action/todo_list/lib/todo_list.ex
@@ -1,2 +1,9 @@
 defmodule TodoList do
+  def new do
+    Map.new
+  end
+
+  def add(todo_list, date, value) do
+    Map.put(todo_list, date, value)
+  end
 end
--- elixir-in-action/todo_list/test/todo_list_test.exs
+++ elixir-in-action/todo_list/test/todo_list_test.exs
@@ -2,7 +2,24 @@ defmodule TodoListTest do
   use ExUnit.Case
   doctest TodoList
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "Can create a new todo list" do
+    assert TodoList.new() == %{}
+  end
+
+  test "Can add a new item to an empty todo list" do
+    expected = %{{2016, 5, 15} => "First item"}
+    actual = TodoList.new |>
+      TodoList.add({2016, 05, 15}, "First item")
+
+    assert actual == expected
+  end
+
+  test "Can add a new item to a populated todo list" do
+    expected = %{{2016, 5, 15} => "First item", {2016, 5, 16} => "Second item"}
+    actual = TodoList.new |>
+      TodoList.add({2016, 05, 15}, "First item") |>
+      TodoList.add({2016, 05, 16}, "Second item")
+
+    assert actual == expected
   end
 end
